96f586d02457f7f3806c92951e0c9bd9779a7ce5
Pedro <pedrobpossidio@gmail.com>
2023-08-16 18:47:25 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('input[name="cep"]').mask('00000-000');
+
+    $('input[name="cpf"]').mask('000.000.000-00');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            telefone: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos Incorretos`)
+            };
+        }
+    });
+})
